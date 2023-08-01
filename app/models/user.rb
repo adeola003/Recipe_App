84c335ed92c294dc9da6aa9c9a92e839f5b7b408
adeola003 <khadaf3@gmail.com>
2023-08-01 19:02:23 +0100

    add custom controller for registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
   has_many :recipes, dependent: :destroy
   has_many :foods, dependent: :destroy
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users, controllers: {
+  registrations: 'users/registrations'
+}
 end
