8a67702ef5eb3745b7a4d2bb9fd4ca0c43b1412a
adeola003 <khadaf3@gmail.com>
2023-08-01 10:49:14 +0100

    run migration

--- db/migrate/20230801084610_create_recipe_foods.rb
+++ db/migrate/20230801084610_create_recipe_foods.rb
@@ -1,6 +1,9 @@
 class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
+      t.integer :quantity
+      t.references :recipe, null: false, foreign_key: true, index: true
+      t.references :food, null: false, foreign_key: true, index: true
 
       t.timestamps
     end
