8881d2e7a42e4ecbd8ec1e46df0287800af142ab
adeola003 <khadaf3@gmail.com>
2023-08-02 08:14:29 +0100

    remove unused code

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,6 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Wrap Devise routes inside a devise_scope block
-  # devise_scope :user do
-  #   get '/users/sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
-  # end
-
-
   resources :recipes, only: [:index, :show]
 
   devise_for :users, controllers: {
