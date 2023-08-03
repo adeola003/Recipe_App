2807b83671a539bbcaf266b312167bb97e3a073e
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-08-03 12:36:21 +0500

    Fix ruboco errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,9 +6,9 @@ class Recipe < ApplicationRecord
 
   def toggle_public
     update(public: !public)
-end
+  end
 
-def add_to_shopping_list
-  update(add_to_shopping_list: !add_to_shopping_list)
-end
+  def add_to_shopping_list
+    update(add_to_shopping_list: !add_to_shopping_list)
+  end
 end
